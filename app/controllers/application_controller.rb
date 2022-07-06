418aad43bf25c36e1c570df78a5637a394dcb75b
Lior <levydlior@gmail.com>
2022-07-06 12:03:36 -0600

    add routes to user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,14 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, "application/json"
 
+  #messages CRUD: 
+
   get "/message_board" do
     messages = Message.all.order(:created_at)
     messages.to_json
   end
 
-  post "/login" do
-    user = User.find_by(user_name: params[:userName], password: params[:password])
-    user.to_json
-  end
+ 
 
   post "/message/post" do
     message = Message.create(content: params[:content], user_id: params[:userId])
@@ -28,4 +27,22 @@ class ApplicationController < Sinatra::Base
     message.destroy
     message.to_json
   end
+
+  #user:
+  post "/user/login" do
+    user = User.find_by(user_name: params[:userName], password: params[:password])
+    user.to_json
+  end
+
+  post "/user/create" do
+    user = User.create(user_name: params[:userName], password: params[:password], avatar_url: params[:image])
+    user.to_json
+  end
+
+  delete "/user/delete/:id" do
+    user = User.find(params[:id])
+    user.destroy
+    user.to_json
+  end
+
 end
