da520cfddfa0313968c05ac4d7abcf15394e0e5d
Lior <levydlior@gmail.com>
2022-07-06 11:51:22 -0600

    add patch,post routes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,8 +1,25 @@
 class ApplicationController < Sinatra::Base
   set :default_content_type, "application/json"
 
+  get "/message_board" do
+    messages = Message.all.order(:created_at)
+    messages.to_json
+  end
+
   post "/login" do
     user = User.find_by(user_name: params[:userName], password: params[:password])
     user.to_json
+  end
+
+  post "/message/post" do
+    message = Message.create(content: params[:content], user_id: params[:userId])
+    message.to_json
+  end
+
+  patch "/message/update" do
+    user = User.find(params[:userId])
+    message = user.messages.find(params[:id])
+    message.update(content: params[:content])
+    message.to_json
   end
 end
